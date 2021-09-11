ed8372f3229d2b617e39318e867a0fd0c1f5c6a7
NamrataMishra97 <89586818+NamrataMishra97@users.noreply.github.com>
2021-09-11 11:33:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,37 +4,36 @@ import numpy as  np
 X=np.array([-6,8])
 # Y vector contain Y1,Y2
 Y=np.array([8,-6])
-m=3
-n=1
+m=1
+n=3
 c= float(m/(m+n))
 d= float(n/(m+n))
+
 a_arr=c*np.array([-6,8])
 b_arr=d*np.array([8,-6])
-out_arr= np.add(a_arr, b_arr)
+out_arr= (a_arr + b_arr)
 print(out_arr)
-print(a_arr)
 r=1
 s=1
 p= float(r/(r+s))
 q= float(s/(r+s))
 e_arr=p*np.array([-6,8])
 f_arr=q*np.array([8,-6])
-out_arr= np.add(e_arr, f_arr)
+out_arr=(e_arr + f_arr)
 print(out_arr)
-u=1
-v=3
+u=3
+v=1
 i= float(u/(u+v))
 j= float(v/(u+v))
 k_arr=i*np.array([-6,8])
 l_arr=j*np.array([8,-6])
-out_arr= np.add(k_arr, l_arr)
+out_arr= (k_arr + l_arr)
 print(out_arr)
-
 
 
 X=([-6,-2.5,1,4.5,8])
 Y=([8,4.5,1,-2.5,-6])
-plt.plot(X,Y,color='green',marker='o')
+plt.plot(X,Y,color='blue',marker='o')
 # plt.scatter(a_arr,e_arr,k_arr)
 # plt.scatter(b_arr,f_arr,l_arr)
 plt.xlabel('x-axis')
